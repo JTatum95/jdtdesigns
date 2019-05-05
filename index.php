00e26c5e05673f44f33a72f13630f95e7502fc28
JTatum95 <Jessetat95@gmail.com>
2019-05-05 17:41:09 -0500

    Edit Favicon

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
     <head>
         <!-- Latest compiled and minified CSS -->
-		<link rel="shortcut icon" href="/favicon.ico">
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 

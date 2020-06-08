c3dc60cd0c7d05720e0aefc2327403b3c6bec897
Jesse Tatum <jessetat95@gmail.com>
2020-06-08 14:26:57 -0500

    Fix messed up resource imports and adjust bottom item in odd number grids

--- js/creative.js
+++ js/creative.js
@@ -1,69 +1,45 @@
-(function($) {
-  "use strict"; // Start of use strict
-
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 72)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
+// Smooth scrolling using jQuery easing
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: (target.offset().top - 72)
+      }, 1000, "easeInOutExpo");
+      return false;
     }
-  });
+  }
+});
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+// Closes responsive menu when a scroll trigger link is clicked
+$('.js-scroll-trigger').click(function() {
+  $('.navbar-collapse').collapse('hide');
+});
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 75
-  });
+// Activate scrollspy to add active class to navbar items on scroll
+$('body').scrollspy({
+  target: '#mainNav',
+  offset: 75
+});
 
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-scrolled");
-    } else {
-      $("#mainNav").removeClass("navbar-scrolled");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
-  // init Masonry
-  // var $grid = $('.grid').masonry({
-  //   // set itemSelector so .grid-sizer is not used in layout
-  //   itemSelector: '.grid-item',
-  //   // use element for option
-  //   columnWidth: '.grid-sizer',
-  //   percentPosition: true
-  // });
-
-  var $grid = $('.grid').masonry({
-    // set itemSelector so .grid-sizer is not used in layout
-    itemSelector: '.grid-item',
-    percentPosition: true,
-    masonry: {
-      // use element for option
-      columnWidth: '.grid-sizer'
-    }
-  // layout Masonry after each image loads
-  $grid.imagesLoaded().progress(function() {
-    $grid.masonry();
-  });
+// Collapse Navbar
+var navbarCollapse = function() {
+  if ($("#mainNav").offset().top > 100) {
+    $("#mainNav").addClass("navbar-scrolled");
+  } else {
+    $("#mainNav").removeClass("navbar-scrolled");
+  }
+};
+// Collapse now if page is not at top
+navbarCollapse();
+// Collapse the navbar when page is scrolled
+$(window).scroll(navbarCollapse);
 
 
+$(document).ready(function() {
   // Magnific popup calls
-  $('.portfolio-wrapper').magnificPopup({
+  $('#portfolio').magnificPopup({
     delegate: 'a',
     type: 'image',
     tLoading: 'Loading image #%curr%...',
@@ -78,4 +54,17 @@
     }
   });
 
-})(jQuery); // End of use strict
+  // init Masonry
+  var $grid = $('.grid').masonry({
+    // set itemSelector so .grid-sizer is not used in layout
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    // use element for option
+    columnWidth: '.grid-sizer'
+  });
+  // layout Masonry after each image loads
+  $grid.imagesLoaded().progress(function () {
+    $grid.masonry();
+  });
+});
+

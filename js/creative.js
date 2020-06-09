ba0a9e67f44f61bba6c827142e725010297095be
Jesse Tatum <jessetat95@gmail.com>
2020-06-09 11:54:25 -0500

    Remove magnific popup from web section, switch to thumbs, add link to site

--- js/creative.js
+++ js/creative.js
@@ -39,20 +39,24 @@ $(window).scroll(navbarCollapse);
 
 $(document).ready(function() {
   // Magnific popup calls
-  $('#portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
+    $('.art-grid').magnificPopup({
+      delegate: 'a',
+      type: 'image',
+      tLoading: 'Loading image #%curr%...',
+      mainClass: 'mfp-img-mobile',
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1]
+      },
+      image: {
+        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+        titleSrc: function (item) {
+          return item.img.attr('alt') || '';
+        }
+      }
   });
+
 
   // init Masonry
   var $grid = $('.grid').masonry({

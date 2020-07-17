8016077caf3d448e7df6f426e924e1c442fabb0f
Jesse Tatum <jessetat95@gmail.com>
2020-07-17 15:31:18 -0500

    Add new art, update overlay for web links

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,22 +1,31 @@
-$( document ).ready(function() {
-    // $popup = $('.grid-item:not(.isotope-hidden)');
-    // $popup.magnificPopup({
-    //     delegate: 'a:not(.site-link)',
-    //     type: 'image',
-    //     tLoading: 'Loading image #%curr%...',
-    //     mainClass: 'mfp-img-mobile',
-    //     gallery: {
-    //         enabled: true,
-    //         navigateByImgClick: true,
-    //         preload: [0, 1]
-    //     },
-    //     image: {
-    //         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-    //         titleSrc: function (item) {
-    //             return item.img.attr('alt') || '';
-    //         }
-    //     }
+$(document).ready(function () {
+    // $all = $('.grid-item:not(.web):visible');
+    // $design = $('.grid-item .design');
+    // $photo = $('.grid-item .photo');
+    // $type = $('.grid-item .type');
+    // var grids = [$all, $design, $photo, $type];
+
+    // $.each(grids, function(i, val) {
+
+    $('.grid-item:not(.web)').magnificPopup({
+        delegate: 'a', // Default for all items
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.img.attr('alt') || '';
+            }
+        }
+    });
     // });
+
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 50) {
@@ -27,6 +36,7 @@ $( document ).ready(function() {
     });
 
     var $grid = $('.grid').isotope({
+        filter: '*',
         // main isotope options
         itemSelector: '.grid-item',
         // set layoutMode
@@ -38,6 +48,10 @@ $( document ).ready(function() {
         }
     });
 
+    $('.shuffle').on( 'click', function() {
+        $grid.isotope('shuffle');
+    });
+
 // layout Masonry after each image loads
     $grid.imagesLoaded().progress(function () {
         $grid.isotope('layout');
@@ -47,6 +61,21 @@ $( document ).ready(function() {
         var filterValue = $(this).attr('data-filter');
         $grid.isotope({filter: filterValue});
         $(this).addClass('active').siblings().removeClass('active');
+
+        // $.each(grids, function(i, val){
+        //     if(i == filterValue.substr(1))
+        //         val.show();
+        //     else
+        //         val.hide();
+        // });
+
+
+        // $popup.magnificPopup({
+        //     delegate: "" + $('a').parent()
+        // });
+        // if(filterValue == "*"){
+        // make headings visible
+        // }
         // this.backgroundColor = "#6c757d";
     });
 });

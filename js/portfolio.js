92c3c538f8e01a3fd12d7b0f70e4d39546935c84
Jesse Tatum <jessetat95@gmail.com>
2020-07-18 17:36:06 -0500

    Remove MP on portfolio, add updated items, remove comments, set to prod

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,31 +1,4 @@
-$(document).ready(function () {
-    // $all = $('.grid-item:not(.web):visible');
-    // $design = $('.grid-item .design');
-    // $photo = $('.grid-item .photo');
-    // $type = $('.grid-item .type');
-    // var grids = [$all, $design, $photo, $type];
-
-    // $.each(grids, function(i, val) {
-
-    $('.grid-item:not(.web)').magnificPopup({
-        delegate: 'a', // Default for all items
-        type: 'image',
-        tLoading: 'Loading image #%curr%...',
-        mainClass: 'mfp-img-mobile',
-        gallery: {
-            enabled: true,
-            navigateByImgClick: true,
-            preload: [0, 1]
-        },
-        image: {
-            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-            titleSrc: function (item) {
-                return item.img.attr('alt') || '';
-            }
-        }
-    });
-    // });
-
+$(document).ready(function () {git
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > 50) {
@@ -58,30 +31,8 @@ $(document).ready(function () {
     });
 
     $('.filter-button-group').on('click', 'button', function () {
-        $mp = $.magnificPopup.instance;
         var filterValue = $(this).attr('data-filter');
         $grid.isotope({filter: filterValue});
         $(this).addClass('active').siblings().removeClass('active');
-        console.log("Before " + $mp.items);
-        $mp.items = $.find(filterValue);
-        console.log("After " + $mp.items);
-
-        $mp.updateItemHTML();
-
-        // $.each(grids, function(i, val){
-        //     if(i == filterValue.substr(1))
-        //         val.show();
-        //     else
-        //         val.hide();
-        // });
-
-
-        // $popup.magnificPopup({
-        //     delegate: "" + $('a').parent()
-        // });
-        // if(filterValue == "*"){
-        // make headings visible
-        // }
-        // this.backgroundColor = "#6c757d";
     });
 });

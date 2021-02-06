be0536c93873b9a2a71154274c89c20432da5f27
Jesse Tatum <jessetat95@gmail.com>
2021-02-05 18:35:26 -0600

    Update main preview page of works

--- js/portfolio.js
+++ js/portfolio.js
@@ -35,4 +35,20 @@ $(document).ready(function () {
         $grid.isotope({filter: filterValue});
         $(this).addClass('active').siblings().removeClass('active');
     });
+
+    // Single image popup boxes for filtering
+    $('.img-fluid').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't forget to change the duration also in CSS
+        }
+    });
 });
